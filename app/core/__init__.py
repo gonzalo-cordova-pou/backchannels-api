5892232fa00f3a56f09ea336c62c0545608301a5
gonzalo-cordova-pou <gonzalo.cordova@glovoapp.com>
2025-07-27 03:03:30 +0200

    feat: set up fastapi project

---
+++ app/core/__init__.py
@@ -0,0 +1,1 @@
+# Core module for the backchannels API
---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from .base import BackchannelModel, PredictionResult
+from .baseline import BaselineModel
+from .distilbert import DistilBertModel
+
+__all__ = [
+    "BackchannelModel",
+    "PredictionResult",
+    "BaselineModel",
+    "DistilBertModel",
+]
